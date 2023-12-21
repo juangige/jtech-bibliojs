f6642248476f9950df480a345312e34e03ec5b6f
Juan <juan.gige95@gmail.com>
2023-12-21 04:35:04 -0300

    entrega

---
+++ main.js
@@ -0,0 +1,73 @@
+alert("Bienvenido, vamos a pedirte unos datos")
+
+let nameUser = prompt("Ingrese su nombre")
+let apellidoUser = prompt("Ingrese su apellido")
+
+alert("Hola " + nameUser + " " + apellidoUser + "!")
+console.log("Bienvenido " + nameUser + " " + apellidoUser + "!")
+
+
+class biblioteca{
+    constructor(nombre, autor, paginas){
+        this.nombre= nombre;
+        this.autor= autor;
+        this.paginas= paginas;
+    }
+}
+
+
+alert("Vamos a ingresar libros!")
+
+
+let nombreLibro = 0
+let autorLibro = 0
+let paginasLibro = 0
+let continuar = 0
+
+
+const arrayBiblio = []
+
+
+while(true){
+    nombreLibro = prompt("Ingrese el nombre")
+    autorLibro = prompt("Ingrese el autor")
+    paginasLibro = parseInt(prompt("Ingrese la cantidad de paginas"))
+
+    const libroNuevo = new biblioteca(nombreLibro, autorLibro, paginasLibro) 
+    arrayBiblio.push(libroNuevo) 
+
+    continuar = prompt("Quiere agregar mas libros? (si/no)")
+    
+    if(continuar.toLowerCase() !== "si"){
+        console.log("No se agregan mas libros!")
+        break
+    }
+}
+
+
+
+let buscar = parseInt(prompt("Buscar libro por cantidad de paginas!"))
+const filtrado1 = arrayBiblio.filter((libro) => libro.paginas === buscar)
+
+
+console.log("Resultado Busqueda")
+filtrado1.forEach((libro)=>{
+console.log(`
+    Libro encontrado: ${libro.nombre}, Autor: ${libro.autor}, Paginas: ${libro.paginas}
+    `)
+})
+
+
+function mostrarLibros(visualizacion) {
+    console.log("===== BIBLIOTECA =====")
+    for (let i = 0; i < arrayBiblio.length; i++) {
+        const biblio = arrayBiblio[i]
+        visualizacion(biblio);
+    }
+}
+
+function mostrarInfo(libro) {
+    console.log(`Nombre: ${libro.nombre}, Autor: ${libro.autor}, Paginas: ${libro.paginas}`);
+}
+
+mostrarLibros(mostrarInfo)
